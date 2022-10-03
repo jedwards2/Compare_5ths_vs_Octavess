ccbbf1cb8530d7f24e057e58916210713f9d1418
Jack <jack.morgan.edwards@gmail.com>
2022-10-03 16:21:03 -0400

    initial commit

--- Comma_Generator/main.cpp
+++ Comma_Generator/main.cpp
@@ -1,14 +1,22 @@
-//
-//  main.cpp
-//  Comma_Generator
-//
-//  Created by Jack Edwards on 10/3/22.
-//
-
 #include <iostream>
+#include <math.h>
+
+double cent_calc(double ratio);
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  std::cout << "Hello, World!\n";
+  double num_ratio{1.5};
+  double den_ratio{2.0};
+  
+  for (int i{2}; i<55; i++){
+    for (int q{2}; q<55; q++){
+      std::cout << "Num Exp: " << i << " Den Exp: " << q << " Ratio " << cent_calc(pow(num_ratio, i) / pow(den_ratio, q)) << std::endl;
+    }
+  }
+  
   return 0;
+}
+
+
+double cent_calc(double ratio){
+  return 1200 * (log10(ratio)/log10(2));
 }
